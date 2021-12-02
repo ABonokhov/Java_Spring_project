757a6ab9930f1418ed5680f3e4614fec8284f47e
ABonokhov <43575362+ABonokhov@users.noreply.github.com>
2021-12-02 14:21:20 +0300

    Add files via upload

---
+++ Java_SpringBoot/WebSite/src/main/java/com/example/WebSite/controllers/PetsController.java
@@ -0,0 +1,13 @@
+package com.example.WebSite.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PetsController {
+    @GetMapping("/petspage")
+    public String petsMain(Model model){
+        return "petsPage";
+    }
+}
